6e8f11c7d5f59c8873b1579fe5f6dfda30276323
Jason Finneyfrock <jfinneyfrock@gmail.com>
2014-06-18 07:52:51 -0400

    purtied up, add/remove etc

--- todont.js
+++ todont.js
@@ -1,50 +1,138 @@
-// override sync to use localStorage
-Backbone.sync = function(method, model) {
-  model.set('id', model.cid);
-  console.log(method + ': ' + JSON.stringify(model));
-}
+$(function() {
 
-var ToDontView = Backbone.View.extend({
+  var ToDont = Backbone.Model.extend({
+    defaults: {
+      'title': "Ain't no titles here",
+      'dirtydog': false
+    }
+  });
 
-});
+  var ToDontList = Backbone.Collection.extend({
 
-var ToDontListView = Backbone.View.extend({
+    model: ToDont,
 
-});
+    initialize: function() {
+      this.listenTo(this, 'change destroy', this.sync);
+    },
 
-var ToDont = Backbone.Model.extend({
+    done: function() {
+      return this.filter(function(model) {
+        return model.get('dirtydog') === true;
+      });
+    },
 
-  // default properties of the model
-  defaults: {
-    'title': 'No title',
-    'completed': false,
-    'dateCompleted': null
-  }
+    undone: function() {
+      return this.filter(function(model) {
+        return model.get('dirtydog') === false;
+      });
+    },
 
-});
+    left: function() {
+      return this.models.length - this.done().length;
+    },
 
-var ToDontList = Backbone.Collection.extend({
+    // delegate to localStorage for saves
+    sync: function() {
+      if (this.models.length === 0) localStorage.removeItem('todont');
+      else localStorage['todont'] = JSON.stringify(this.toJSON());
+    }
+  });
 
-  // specify the model that backs this collection
-  model: ToDont,
+  var ToDontView = Backbone.View.extend({
 
-  // filter to all completed items
-  completed: function() {
-    return this.filter(function(model) {
-      return model.get('completed');
-    });
-  },
+    // tag for new item
+    tagName: 'li',
 
-  // filter to all incomplete items
-  uncompleted: function() {
-    return this.filter(function(model) {
-      return model.get('completed') === false
-    });
-  }
-});
+    // cache the template func
+    template: _.template($('#todont').html()),
 
-$(function(){
-  // add/remove/complete items in view
-  // show meta about list (remaining etc)
-  // sync to localstorage
+    events: {
+      'click input[type=checkbox]': 'toggle',
+      'click a': 'destroy'
+    },
+
+    render: function() {
+      var html = this.template(this.model.toJSON());
+      this.$el.append(html);
+      if (this.model.get('dirtydog')) {
+        this.$el.addClass('dirtydog');
+        this.$el.find('input').attr('checked', true);
+      }
+      return this;
+    },
+
+    toggle: function(e) {
+      this.model.set('dirtydog', e.target.checked);
+      this.$el.toggleClass('dirtydog');
+    },
+
+    destroy: function(e) {
+      e.preventDefault();
+      this.model.destroy();
+    }
+  });
+
+  var AppView = Backbone.View.extend({
+
+    // bind to existing DOM
+    el: $('body'),
+
+    events: {
+      'keypress input[name=entry]': 'create'
+    },
+
+    initialize: function() {
+
+      // cache these since they're used repeatedly
+      this.$ul = this.$('ul');
+      this.$header = this.$('header');
+      this.$footer = this.$('footer');
+
+      this.donts = new ToDontList();
+      this.listenTo(this.donts, 'add destroy', this.render);
+      this.listenTo(this.donts, 'all', this.reckoning);
+      this.listenTo(this.donts, 'all', this.footer);
+
+      // nothing stored - seed data
+      if (localStorage['todont'] == null) {
+        this.donts.add(new ToDont({ title: "Covet thy neighbor's shizz" }));
+        this.donts.add(new ToDont({ title: "Worship graven images" }));
+        this.donts.add(new ToDont({ title: "Pick thy nose" }));
+      }
+      else {
+        var items = JSON.parse(localStorage['todont']);
+        _.each(items, function(item) {
+          this.donts.add(new ToDont(item));
+        }, this);
+        this.footer();
+      }
+    },
+
+    render: function() {
+      this.$ul.empty();
+      this.donts.each(function(todont){
+        var view = new ToDontView({ model: todont });
+        this.$ul.append(view.render().el);
+      }, this); // note the passed in context
+    },
+
+    reckoning: function() {
+      this.$header.empty();
+      if (this.donts.left() == 0) this.$header.html('<h2>You dirty dog!</h2>');
+    },
+
+    footer: function() {
+      this.$footer.empty();
+      this.$footer.html('Saved: <code>' + JSON.stringify(this.donts.toJSON()) + '</code>');
+    },
+
+    create: function(e) {
+      if (e.keyCode !== 13) return;
+      this.donts.add(new ToDont({ title: e.target.value }));
+      e.target.value = '';
+    }
+  });
+
+  var app = new AppView();
+
 });
